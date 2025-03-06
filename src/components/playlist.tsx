efda40a9402b893774c3928fac4cd0ccc628218b
nathertan <ndihyan@gmail.com>
2025-03-06 13:03:10 +0700

    feat: added shuffle to playlist

--- src/components/playlist.tsx
+++ src/components/playlist.tsx
@@ -25,16 +25,20 @@ const Playlist: React.FC<PlaylistProps> = ({ onVideoSelect }) => {
         const fetchPlaylist = async () => {
             try {
                 const response = await axios.get(API_URL);
-                const fetchedVideos = response.data.items.map((item: any) => ({
+                let fetchedVideos = response.data.items.map((item: any) => ({
                     title: item.snippet.title,
                     videoId: item.snippet.resourceId.videoId,
                     thumbnail: item.snippet.thumbnails.standard.url,
                 }));
+
+                //shuffling playlist
+                fetchedVideos = fetchedVideos.sort(() => Math.random() - 0.5);
                 setVideos(fetchedVideos);
             } catch (error) {
                 console.error("Error fetching playlist:", error);
             }
         };
+        //run the playlist get
         fetchPlaylist();
         
     }, []);
